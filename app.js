dea06f10b4d3397b635865ef2c4cd125637c0b38
Trugamr <6trugamr9@gmail.com>
2019-08-27 00:18:41 +0530

    =added geocoding api support

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 require('dotenv').config();
 const request = require('request-promise');
 
-const API_KEY = process.env.WEATHER_API_KEY;
-const BASE_URL = `https://api.darksky.net/forecast/${ API_KEY }/`;
-const coords = `30.7327891,76.7627217`;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const WEATHER_BASE_URL = `https://api.darksky.net/forecast/${ WEATHER_API_KEY }/`;
+const GEOCODE_BASE_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/`
+let coords = `30.7327891,76.7627217`;
 
 const api = request.defaults({
-    baseUrl: BASE_URL,
-    json: true
+    baseUrl: WEATHER_BASE_URL,
+    json: true,
+    qs: {
+        units: 'si',
+        lang: 'en'
+    }
+})
+
+const geocode = request.defaults({
+    baseUrl: GEOCODE_BASE_URL,
+    json: true,
+    qs: {
+        access_token: GEOCODE_API_KEY,
+        limit: 1
+    }
 })
 
 api({ url: `/${coords}` })
     .then(data => {
         const { precipProbability, temperature } = data.currently;
-        console.log(`Is's currenty ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`);
+        const { summary } = data.daily.data[0];
+        console.log(`${summary} Is's currenty ${temperature} degrees out. There is a ${precipProbability * 100}% chance of rain.`);
     })
     .catch(err => console.error(err));
+
+geocode({ url: `/chandigarh.json` })
+    .then(data => {
+        const { center } = data.features[0];
+        const [ longitude, latitude ] = center;
+        console.log(`${ longitude }, ${ latitude }`);
+    })
