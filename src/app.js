a3757598cba49edc2f114b57590335bba1da73bd
Trugamr <6trugamr9@gmail.com>
2020-03-20 11:26:11 +0530

    updated api responses to return full data

--- src/app.js
+++ src/app.js
@@ -1,86 +1,82 @@
-require('dotenv').config()
-const path = require('path')
-const express = require('express')
-const { getWeather } = require('../utils/weather')
-const { getGeoCode } = require('../utils/geoCode')
-const hbs = require('hbs')
-
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const { getWeather } = require("../utils/weather");
+const { getGeoCode } = require("../utils/geoCode");
+const hbs = require("hbs");
 
 // let coords = {
 //     longitude: 76.779419,
 //     latitude: 30.7327891
 // }
 
-const app = new express()
-const PORT = process.env.PORT || 3000
+const app = new express();
+const PORT = process.env.PORT || 3000;
 
 // define paths for express config
-const publicPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // set handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // setup static directory
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
+app.get("/", (req, res) => {
+  res.render("index", { title: "Weather" });
+});
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Weather' })
-})
-
-app.get('/weather', async (req, res) => {
-    const { search } = req.query
-    if(search) {
-        try {
-            const data = await getForecast(search)
-            data.search = search
-            return res.send(data)
-        } catch(err) {
-            return res.send({
-                error: err.message
-            })            
-        }
+app.get("/weather", async (req, res) => {
+  const { search } = req.query;
+  if (search) {
+    try {
+      const data = await getForecast(search);
+      data.search = search;
+      return res.send(data);
+    } catch (err) {
+      return res.send({
+        error: err.message
+      });
     }
+  }
 
-    res.status(200).send({
-        error: "You must provide a search term."
-    })
-    
-})
+  res.status(200).send({
+    error: "You must provide a search term."
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About Me'})
-})
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Me" });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', { title: 'Help', msg: 'Why are you here ?' });
-})
+app.get("/help", (req, res) => {
+  res.render("help", { title: "Help", msg: "Why are you here ?" });
+});
 
 // 404 page
-app.get('/help/*', (req, res) => {
-    res.render('404-page', { error: '404 - Help article not found' })
-})
+app.get("/help/*", (req, res) => {
+  res.render("404-page", { error: "404 - Help article not found" });
+});
 
-app.get('*', (req, res) => {
-    res.render('404-page', { title:'404', error: '404 - Page not found.'})
-})
+app.get("*", (req, res) => {
+  res.render("404-page", { title: "404", error: "404 - Page not found." });
+});
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
+const getForecast = async location => {
+  try {
+    const coords = await getGeoCode(location);
+    const weather = await getWeather(coords);
+    return { ...coords, ...weather };
+  } catch (err) {
+    throw err;
+  }
+};
 
-const getForecast = async (location) => {
-    try {
-        const coords = await getGeoCode(location)
-        const forecast = await getWeather(coords)
-        return { ...coords, forecast }
-    } catch(err) {
-        throw(err)
-    } 
-}
-
-if(process.argv[2]) getForecast(process.argv[2])
-else console.log('Please provide a location.')
+if (process.argv[2]) getForecast(process.argv[2]);
+else console.log("Please provide a location.");
--- utils/weather.js
+++ utils/weather.js
@@ -1,26 +1,31 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getWeather = async ({ longitude, latitude }) => {
-    const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
-    const WEATHER_BASE_URL = `https://api.darksky.net/forecast/${ WEATHER_API_KEY }/`;  
-    const options = {
-        url: `${latitude},${longitude}`,
-        baseUrl: WEATHER_BASE_URL,
-        json: true,
-        qs: {
-            units: 'si',
-            lang: 'en'
-        }
+  const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+  const WEATHER_BASE_URL = `https://api.darksky.net/forecast/${WEATHER_API_KEY}/`;
+  const options = {
+    url: `${latitude},${longitude}`,
+    baseUrl: WEATHER_BASE_URL,
+    json: true,
+    qs: {
+      units: "si",
+      lang: "en"
     }
-    try {
-        const data = await request(options);
-        const { precipProbability, temperature } = data.currently;
-        const { summary } = data.daily.data[0];
-        return (`${summary} Is's currenty ${temperature} degrees out. There is a ${(precipProbability * 100).toPrecision(2)}% chance of rain.`);
-    } catch(err) {
-        if(err.statusCode) throw(new Error('Location not found.'));
-        else throw(new Error('Unable to connect to weather service.'));
-    }
-}
+  };
+  try {
+    const data = await request(options);
+    const { precipProbability, temperature } = data.currently;
+    const { summary } = data.daily.data[0];
+    return {
+      forecast: `${summary} Is's currenty ${temperature} degrees out. There is a ${(
+        precipProbability * 100
+      ).toPrecision(2)}% chance of rain.`,
+      ...data
+    };
+  } catch (err) {
+    if (err.statusCode) throw new Error("Location not found.");
+    else throw new Error("Unable to connect to weather service.");
+  }
+};
 
-module.exports = { getWeather }
+module.exports = { getWeather };
